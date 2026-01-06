836621c60c73bf47e77dd6ba626b9cfa7c84f9b8
Houssem SAKLI <sakli.houssem@yahoo.com>
2026-01-06 17:49:47 +0100

    initialized project

---
+++ src/main/java/com/rhis/solutions/GameTestApplication.java
@@ -0,0 +1,13 @@
+package com.rhis.solutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rhis/solutions/GameTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhis.solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
